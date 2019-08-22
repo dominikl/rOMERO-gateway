a691d0172731ac768a51eeccd0fd36fc365df536
Dominik Lindner <d.lindner@dundee.ac.uk>
2019-08-22 13:58:06 +0100

    test

--- install.R
+++ install.R
@@ -7,4 +7,8 @@
 #install.packages("rJava")
 #install.packages("devtools")
 #install.packages("https://github.com/ome/rOMERO-gateway/releases/download/v0.4.7/romero.gateway_0.4.7.tar.gz", repos = NULL, type = "source")
-
+install.packages("tidyverse")
+install.packages("rmarkdown")
+install.packages("httr")
+install.packages("shinydashboard")
+install.packages('leaflet')
