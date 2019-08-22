0180663489541417820cd819612d5f758015fe8f
Dominik Lindner <d.lindner@dundee.ac.uk>
2019-08-22 10:51:35 +0100

    Don't install anything yet

--- install.R
+++ install.R
@@ -1,10 +1,10 @@
 # Only used for mybinder.
 # For building/installing the romero.gateway package
 # see install_package.R
-install.packages("methods")
-install.packages("jpeg")
-install.packages("httr")
-install.packages("rJava")
-install.packages("devtools")
-install.packages("https://github.com/ome/rOMERO-gateway/releases/download/v0.4.7/romero.gateway_0.4.7.tar.gz", repos = NULL, type = "source")
+#install.packages("methods")
+#install.packages("jpeg")
+#install.packages("httr")
+#install.packages("rJava")
+#install.packages("devtools")
+#install.packages("https://github.com/ome/rOMERO-gateway/releases/download/v0.4.7/romero.gateway_0.4.7.tar.gz", repos = NULL, type = "source")
 
