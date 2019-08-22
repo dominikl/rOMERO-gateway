97536701bc18ee31520339db815144de620cbf78
Dominik Lindner <d.lindner@dundee.ac.uk>
2019-08-22 10:14:54 +0100

    Add some necessary packages to install.R

--- install.R
+++ install.R
@@ -1,4 +1,9 @@
 # Only used for mybinder.
 # For building/installing the romero.gateway package
 # see install_package.R
+install.packages("methods")
+install.packages("jpeg")
+install.packages("httr")
+install.packages("rJava")
+install.packages("devtools")
 
